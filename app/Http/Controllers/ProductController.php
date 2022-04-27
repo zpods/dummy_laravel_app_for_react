2c8883571289dd51b3ce24738cd914c01cb65d52
zp <zbyszek_p1@o2.pl>
2022-04-27 08:33:40 +0200

    create ProductController.php with two actions index to show all products and searchProduct to search on product add to routes/api.php new routs for ProductsController searchProduct and index

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::with(['images'])->get()->take(6);
+        return response()->json($products);
+    }
+
+    /**
+     * Display searched products.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function searchProducts($name)
+    {
+        $result = Product::where('name', '=', $name)->with('images')->get();
+        if($result){
+            return response()->json($result, 200);
+        }else{
+            return response()->json('Products not found', 404);
+        }
+
+    }
+}
